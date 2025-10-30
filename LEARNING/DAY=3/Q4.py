4c7595285072f3e097e73fa991c374e21b40a794
kaurmehar1408-tech <kaurmehar1408@gmail.com>
2025-10-30 20:22:45 +0530

    git push

---
+++ LEARNING/DAY=3/Q4.py
@@ -0,0 +1,7 @@
+# Q4. Write a program to print even numbers from 2 to 20 using a for loop in python
+# Expected Output:
+# 2 4 6 8 10 12 14 16 18 20
+
+for i in range(1,21):
+    if i%2==0:
+        print(i)
---
+++ LEARNING/DAY=3/Q9.py
@@ -0,0 +1,9 @@
+# Q9. Write a program that takes a number n as input and prints numbers from 1 to n using a for loop.
+# python
+# Sample Input: 7
+# Expected Output: 1 2 3 4 5 6 7
+
+n = int(input("Enter a number: "))
+for i in range(1,n+1):
+    print(i)
+    
